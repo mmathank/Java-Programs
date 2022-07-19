b320b2f33047921d7b1273d62b2c86758e7f99a1
mmathank <mathan.ambition@gmail.com>
2022-07-19 07:45:17 +0530

    Files Added in Thread Concepts

--- src/thread/concepts/SynchronizedExample.java
+++ src/thread/concepts/SynchronizedExample.java
@@ -1,57 +1,31 @@
 package core.java.thread.concepts;
 
 public class SynchronizedExample {
+    int count = 0;
 
-	int count = 0;
-	
-	public void method1() {
-		synchronized(this) {
-			for (int i = 0; i < 10; i++) {
-				System.out.println(i);
-				count++;
-			}
-			System.out.println("Method 1: " + count);
-		}
-	}
+    public static void main(String[] args) {
 
-	public void method2() {
-		synchronized(this) {
-			for (int i = 0; i < 10; i++) {
-				System.out.println(i);
-				count++;
-			}
-			System.out.println("Method 2: " + count++);
-		}
-	}
+        SynchronizedExample se = new SynchronizedExample();
 
-	public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            se.increment(50);
+        });
 
-		SynchronizedExample se = new SynchronizedExample();
+        Thread t2 = new Thread(() -> {
+            se.decrement(50);
+        });
 
-		Thread t1 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				se.method1();
-			}
-		});
+        t1.start();
+        t2.start();
+    }
 
-		Thread t2 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				se.method2();
-			}
-		});
+    public synchronized void increment(int value) {
+        count += value;
+        System.out.println(count);
+    }
 
-		t1.start();
-		t2.start();
-
-		try {
-			t1.join();
-			t2.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-	}
-
+    public synchronized void decrement(int value) {
+        count -= value;
+        System.out.println(count);
+    }
 }
